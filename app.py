90443baee28cbcbefbd6ff28d3c94b773bd0574e
Sandberg, Nathan <Nathan.Sandberg@cdk.com>
2022-03-25 11:42:44 -0500

    First Commit

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from flask_restful import Resource, Api, reqparse, abort
 
 import json
 
+import jwt
+from jwt import PyJWKClient
+
+PREFIX = 'Bearer '
+
 app = Flask(__name__)
 api = Api(app)
 
@@ -17,8 +22,27 @@ class Event(Resource):
         app.logger.debug('Headers: %s', request.headers)
         app.logger.debug('Request: %s', request.get_data())
 
-        entry = json.loads(request.get_data().decode())
-        
+        token = request.headers['Authorization'][len(PREFIX):]
+
+        url = "https://identity.fortellis.io/oauth2/aus1p1ixy7YL8cMq02p7/v1/keys"
+
+        jwks_client = PyJWKClient(url)
+
+        signing_key = jwks_client.get_signing_key_from_jwt(token)
+
+        data = jwt.decode(
+            token,
+            signing_key.key,
+            algorithms=["RS256"],
+            audience="api_providers",
+            subject="{yourAPIKey}",
+            options={"verify_exp": True},
+        )
+
+        print(data)
+
+        entry = json.loads(request.get_data().decode())  
+
         with open('queue.json', '+r') as file:
             file_data = json.load(file)
             file_data['queue'].append(entry)
@@ -27,6 +51,15 @@ class Event(Resource):
         return ('', 202)
 
 api.add_resource(Event, '/hello/world/event')
+
+class Queue(Resource):
+    def get(self):
+        with open('queue.json', '+r') as file:
+            file_data = json.load(file)
+            
+        return file_data
+
+api.add_resource(Queue, '/hello/world')
 
 if __name__ == '__main__':
     app.run(debug=True)
